a75eb73409ef652b10d805e7be935f764be9ea3f
offorma <offorma@gmail.com>
2019-07-25 17:28:02 +0100

    added functions to create books and store in array

--- js/booklibrary.js
+++ js/booklibrary.js
@@ -6,12 +6,44 @@
     let myLibrary = [];
 
     function Book() {
-    // the constructor...
+        this.read = false;
     }
 
-    function addBookToLibrary() {
-    // do stuff here
+    function addBookToLibrary(book) {
+        myLibrary.concat(book);
+  
+        alert('book added');
+        $('#myModal').modal('hide')
+    }
+    function getFromHtml(){
+        //get all the fields
+        let author = getElement("input[id='author']").value;
+        let title = getElement("input[id='title']").value;
+        let numberOfPages = getElement("input[id='NumberofPages']").value;
+        createBook(author,title,numberOfPages);
+    }
+    function createBook(author,title,numberOfPages){
+        // make sure they are not empty
+        if( author != "" && title != "" && numberOfPages!= ""){
+             let newBook = new Book();
+             newBook.author = author;
+             newBook.title = title;
+             newBook.numberOfPages = numberOfPages;
+             addBookToLibrary(newBook);
+        }else {
+             alert("fields cannot be empty");
+        }
     }
     function render(){
         
     }  
+    function markAsRead(){
+
+    }
+    function deleteBook(){
+
+    }
+    function getElement(id){
+        return document.querySelector(id);
+    }
+   
